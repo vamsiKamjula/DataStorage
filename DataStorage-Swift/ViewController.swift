d3f2c23aff084d317ff16decb2a92ad281900729
vamsi krishna reddy kamjula <vamsikrishnareddykamjula@siddharths-air.fios-router.home>
2017-07-26 12:21:16 -0400

    Initial Commit

---
+++ DataStorage-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataStorage-Swift
+//
+//  Created by vamsi krishna reddy kamjula on 7/26/17.
+//  Copyright © 2017 kvkr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
